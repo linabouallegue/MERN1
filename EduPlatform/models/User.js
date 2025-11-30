2ad5f8413875aa9a488ce3e035bef8e5ccf3333a
Bouallegue Lina <lina.bouallegue@polytechnicien.tn>
2025-11-30 14:55:13 +0100

    update project

---
+++ EduPlatform/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  courses: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Course' 
+  }]
+});
+module.exports = mongoose.model('User', userSchema);
---
+++ EduPlatform/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const User = require('../models/User');
+
+
+// Route publique
+router.get('/', async (req, res) => {
+  // Accessible sans authentification
+  res.json({ message: "Route publique OK" });
+});
+
+// Route protégée
+router.get('/profile', protect, async (req, res) => {
+  // req.userId contient l’ID de l’utilisateur connecté
+  const user = await User.findById(req.userId).select('-password');
+
+  res.json(user);
+});
+
+module.exports = router;
